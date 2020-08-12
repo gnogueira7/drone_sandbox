b5eb474703d2319f4bc51784146247188d07ffb9
Ravi Lachhman <42775804+ravilach@users.noreply.github.com>
2020-08-12 16:34:23 -0400

    Update main.go
    Test

--- main.go
+++ main.go
@@ -1,77 +1,20 @@
-//From https://www.callicoder.com/deploy-containerized-go-app-kubernetes/
-
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
-
-	"github.com/gorilla/mux"
+  "fmt"
+  "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	query := r.URL.Query()
-	name := query.Get("name")
-	if name == "" {
-		name = "Guest"
-	}
-	log.Printf("Received request for %s\n", name)
-	w.Write([]byte(fmt.Sprintf("Hello, %s\n", name)))
+func homePage(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "My Awesome Go Web App")
 }
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
-
-func readinessHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+func setupRoutes() {
+  http.HandleFunc("/", homePage)
 }
 
 func main() {
-	// Create Server and Route Handlers
-	r := mux.NewRouter()
-
-	r.HandleFunc("/", handler)
-	r.HandleFunc("/health", healthHandler)
-	r.HandleFunc("/readiness", readinessHandler)
-
-	srv := &http.Server{
-		Handler:      r,
-		Addr:         ":8080",
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-
-	// Start Server
-	go func() {
-		log.Println("Starting Server")
-		if err := srv.ListenAndServe(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	// Graceful Shutdown
-	waitForShutdown(srv)
-}
-
-func waitForShutdown(srv *http.Server) {
-	interruptChan := make(chan os.Signal, 1)
-	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-
-	// Block until we receive our signal.
-	<-interruptChan
-
-	// Create a deadline to wait for.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	srv.Shutdown(ctx)
-
-	log.Println("Shutting down")
-	os.Exit(0)
+  fmt.Println("Go Web App Started on Port 80")
+  setupRoutes()
+  http.ListenAndServe(":80", nil)
 }
